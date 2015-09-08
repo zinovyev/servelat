0497d8f0b3e588f1c525e2fc6b26042bee626d2f
Ivan Zinovyev <vanyazin@gmail.com>
2015-09-08 08:08:47 +0300

    Base process interface

--- app/supervisor/ProcessInterface.php
+++ app/supervisor/ProcessInterface.php
@@ -4,9 +4,29 @@ namespace servelat\supervisor;
 
 /**
  * Interface ProcessInterface.
- *
+ * Base process interface.
  *
  * @author Ivan Zinovyev <vanyazin@gmail.com>
  */
 interface ProcessInterface
-{}
+{
+    /**
+     * @param \servelat\supervisor\TaskInterface $task
+     */
+    public function __construct(TaskInterface $task);
+
+    /**
+     * Get original task.
+     * Get the task object the process belongs to.
+     *
+     * @return \servelat\supervisor\TaskInterface
+     */
+    public function getTask();
+
+    /**
+     * Is still running.
+     *
+     * @return boolean
+     */
+    public function isRunning();
+}
