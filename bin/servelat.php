9fad298661a73e39089f43ab18b5cf6dda89ca58
Ivan Zinovyev <vanyazin@gmail.com>
2015-10-02 21:43:01 +0300

    Symfony console integration initiated

---
+++ bin/servelat.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * The servelat cli file.
+ *
+ * The servelat console is an entry point of the application.
+ * It is used by both server and client components.
+ *
+ * @author Ivan Zinovyev <vanyazin@gmail.com>
+ */
